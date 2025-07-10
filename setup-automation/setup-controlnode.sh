b59594af45fd68a6ffba4dc5d0306d67dd797737
Matthew Yee <myee@redhat.com>
2025-07-10 13:36:04 -0700

    first commit

--- setup-automation/setup-controlnode.sh
+++ setup-automation/setup-controlnode.sh
@@ -1,18 +1,2 @@
 #!/bin/bash
-while [ ! -f /opt/instruqt/bootstrap/host-bootstrap-completed ]
-do
-    echo "Waiting for Instruqt to finish booting the VM"
-    sleep 1
-done
 
-virsh start vm1
-
-subscription-manager register --activationkey=${ACTIVATION_KEY} --org=12451665 --force
-
-dnf install -y nc tmux
-
-echo "${SSHKEY}" > /root/.ssh/id_rsa
-chmod 600 /root/.ssh/id_rsa
-ssh-keygen -f /root/.ssh/id_rsa -y > /root/.ssh/id_rsa.pub
-
-(r=3;while ! ssh -i /root/.ssh/id_rsa -o "StrictHostKeyChecking no" root@vm1 "subscription-manager register --activationkey=${ACTIVATION_KEY} --org=12451665 --force" ; do ((--r))||exit;sleep 30;done)
